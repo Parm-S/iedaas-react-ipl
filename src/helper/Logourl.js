bb4036b80eb07411ab330fc25d5e55b13cefed63
Parminder Singh <parminder.singh629@gmail.com>
2021-09-13 18:08:22 +0530

    Add all my files

---
+++ src/helper/Logourl.js
@@ -0,0 +1,13 @@
+const LOGO_URL ={
+    'chennai-super-kings':require('../assets/csk.png'),
+    'delhi-capitals':require('../assets/dc.png'),
+    'kings-xi-punjab':require('../assets/pk.png'),
+    'kolkata-knight-riders':require('../assets/kkr.png'),
+    'mumbai-indians':require('../assets/mi.png'),
+    'rajasthan-royals':require('../assets/rr.png'),
+    'royal-challengers-bangalore':require('../assets/rcb.png'),
+    'sunrisers-hyderabad':require('../assets/srh.png')
+}
+
+Object.freeze(LOGO_URL);
+export default LOGO_URL
